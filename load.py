bb684daca9ed48c177d26613eb64ae963a803487
Rares Vernica <rvernica@gmail.com>
2018-05-20 22:37:55 -0700

    Streamline load script

--- load.py
+++ load.py
@@ -17,23 +17,25 @@ while True:
     sys.stderr.write('df_in.dtype[:5]: {}\n'.format(df_in.dtypes[:5]))
     sys.stderr.write('df_in.dtype[-5:]: {}\n'.format(df_in.dtypes[-5:]))
 
-    df_out = pandas.DataFrame(
-      columns='chrom pos p id ref alt qual flt info fmt gt'.split())
-    i = 0
+    lst = []
     for line in df_in.itertuples(index=False):
       for j in range(500):
-        df_out.loc[i] = [line[510], # chrom
-                         line[511], # pos
-                         1 + j,
-                         line[2],   # id
-                         line[3],   # ref
-                         line[4],   # pos
-                         line[512], # qual
-                         line[6],   # flt
-                         line[7],   # info
-                         line[8],   # fmt
-                         line[9 + j]]
-        i += 1
+        lst.append(
+            [line[510], # chrom
+             line[511], # pos
+             1 + j,
+             line[2],   # id
+             line[3],   # ref
+             line[4],   # pos
+             line[512], # qual
+             line[6],   # flt
+             line[7],   # info
+             line[8],   # fmt
+             line[9 + j]])
+
+    df_out = pandas.DataFrame(
+      lst,
+      columns='chrom pos p id ref alt qual flt info fmt gt'.split())
 
     # Write DataFrame
     scidbstrm.write(df_out)
